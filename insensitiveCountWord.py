99dc939c4bd1901b6c10f50b980160c1a4d26cc7
owfdataboy <phanvigiaii@gmail.com>
2022-03-29 05:23:55 +0700

    done

--- insensitiveCountWord.py
+++ insensitiveCountWord.py
@@ -1,23 +1,21 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class mostUsedWord(MRJob):
+class insensitiveCountWord(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_word_count,
-            reducer=self.reducer_word_count),
-            MRStep( reducer=self.reducer_find_max_word)
+            MRStep(mapper=self.mapper_word_count),
+            MRStep(reducer=self.reducer_word_count)
         ]
         
     def mapper_word_count(self, _, line):
         for word in line.split():
             yield(word.lower(), 1)
+
     def reducer_word_count(self, word, counts):
         yield None,(sum(counts), word)
 
-    def reducer_find_max_word(self,_,word_count_pairs):
-        yield max(word_count_pairs)
 
 if __name__=='__main__':
-    mostUsedWord.run()
+    insensitiveCountWord.run()
